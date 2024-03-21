ba23c6a803a9bc910dbf89e1fda55d8f0655a187
Stanislav Korneev <korstasvik@gmail.com>
2024-03-21 13:05:28 +0400

    added Scene, RectWidget and ConnectionButton classes. Widgets appear by dblclick

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton
+from PyQt6.QtGui import QPainter, QBrush, QColor
+from PyQt6.QtCore import Qt, QRect, QSize, QPoint, pyqtSignal
+
 import sys
+
+
+class ConnectionButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent = parent
+        self.size = 15
+        posX = int((parent.width - self.size) / 2)
+        posY = int((parent.height - self.size) / 2)
+        self.setGeometry(posX, posY, self.size, self.size)
+        self.show()
+
+
+class RectWidget(QWidget):
+    def __init__(self, parent=None, pos=QPoint(50, 50)):
+        super().__init__(parent)
+        self.width = 100
+        self.height = 50
+        self.connectionBtn = ConnectionButton(self)
+        posX = int(pos.x() - self.width / 2)
+        posY = int(pos.y() - self.height / 2)
+        self.setGeometry(QRect(QPoint(posX, posY), QSize(self.width, self.height)))
+
+        self.show()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fillRect(self.rect(), QColor('red'))
+
+
+class Scene(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.rects = []
+
+    def addRect(self, pos):
+        rect = RectWidget(self, pos)
+        self.rects.append(rect)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fillRect(self.rect(), QColor('lightBlue'))
+
+    def mouseDoubleClickEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            self.addRect(event.pos())
 
 
 class MainWindow(QWidget):
@@ -18,6 +67,8 @@ class MainWindow(QWidget):
 def application():
     app = QApplication(sys.argv)
     window = MainWindow()
+    scene = Scene()
+    window.layout().addWidget(scene)
 
     window.show()
     sys.exit(app.exec())
