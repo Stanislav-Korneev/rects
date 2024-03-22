e92ae03c4f5239f8aa26ad610c991f769221f314
Stanislav Korneev <korstasvik@gmail.com>
2024-03-22 12:39:08 +0400

    introduces config file

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton
-from PyQt6.QtGui import QPainter, QColor, QPen
-from PyQt6.QtCore import Qt, QRect, QSize, QPoint
-
+import json
+import random
 import sys
+
+from PyQt6.QtCore import Qt, QRect, QSize, QPoint
+from PyQt6.QtGui import QPainter, QColor, QPen
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton
 
 
 class ConnectionButton(QPushButton):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, config=None):
         super().__init__(parent)
-        self.size = 15
+        self.size = config['connectionButtonSize']
         posX = int((parent.width - self.size) / 2)
         posY = int((parent.height - self.size) / 2)
         self.setGeometry(posX, posY, self.size, self.size)
 
 
 class RectWidget(QWidget):
-    def __init__(self, parent=None, pos=QPoint(50, 50)):
+    def __init__(self, parent=None, pos=QPoint(50, 50), config=None):
         super().__init__(parent)
+        self.width = config['rectWidth']
+        self.height = config['rectHeight']
+        self.color = random.choice(config['rectColors'])
         self.linkedRectWidgets = []
-        self.width = 100
-        self.height = 50
         self.lastMousePosition = None
 
-        self.connectionBtn = ConnectionButton(self)
+        self.connectionBtn = ConnectionButton(self, config)
         posX = int(pos.x() - self.width / 2)
         posY = int(pos.y() - self.height / 2)
         self.setGeometry(QRect(QPoint(posX, posY), QSize(self.width, self.height)))
@@ -32,7 +35,7 @@ class RectWidget(QWidget):
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.fillRect(self.rect(), QColor('red'))
+        painter.fillRect(self.rect(), QColor(self.color))
 
     def mousePressEvent(self, event):
         if event.buttons() != Qt.MouseButton.LeftButton:
@@ -56,16 +59,17 @@ class RectWidget(QWidget):
 
 
 class Scene(QWidget):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
         self.connectWidget = None
+        self.config = config
 
     def addRect(self, pos):
-        size = QSize(100, 50)
+        size = QSize(self.config['rectWidth'], self.config['rectHeight'])
         rect = QRect(pos, size)
         if self.hasRectCollisions(None, rect):
             return
-        RectWidget(self, pos)
+        RectWidget(self, pos, self.config)
 
     def hasRectCollisions(self, targetWidget, targetRect):
         hasWindowCollision = not self.rect().contains(targetRect)
@@ -78,11 +82,11 @@ class Scene(QWidget):
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        pen = QPen(QColor('black'))
-        pen.setWidth(5)
+        pen = QPen(QColor(self.config['connectorColor']))
+        pen.setWidth(self.config['connectorWidth'])
         painter.setPen(pen)
 
-        painter.fillRect(self.rect(), QColor('lightBlue'))
+        painter.fillRect(self.rect(), QColor(self.config['sceneBackground']))
         self.drawLines(painter)
 
     def drawLines(self, painter):
@@ -122,22 +126,26 @@ class Scene(QWidget):
 
 
 class MainWindow(QWidget):
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
 
-        self.setWindowTitle("rectangles")
-        self.setGeometry(300, 300, 500, 500)
-        self.setContentsMargins(0, 0, 0, 0)
+        self.setWindowTitle(config['windowTitle'])
+        self.setFixedSize(*config['windowSize'])
+        self.move(*config['windowPosition'])
+        self.setContentsMargins(*config['contentsMargins'])
 
         layout = QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setContentsMargins(*config['contentsMargins'])
         self.setLayout(layout)
 
 
 def application():
+    with open('config.json', 'r') as file:
+        config = json.loads(file.read())
+
     app = QApplication(sys.argv)
-    window = MainWindow()
-    scene = Scene()
+    window = MainWindow(config)
+    scene = Scene(config)
     window.layout().addWidget(scene)
 
     window.show()
