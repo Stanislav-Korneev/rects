f1ea3850007523b221456bcd8442d024165b78b0
Stanislav Korneev <korstasvik@gmail.com>
2024-03-22 16:41:45 +0400

    added favicon

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import sys
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 
 from modules.Scene import Scene
 from modules.MainWindow import MainWindow
@@ -12,6 +13,7 @@ def application():
         config = json.loads(file.read())
 
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('icons/rect_icon.png'))
     window = MainWindow(config)
     scene = Scene(config)
     window.layout().addWidget(scene)
